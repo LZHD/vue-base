61381bb16cafb670705093714941c43470722b24
fanxingqiang <lzhdfxq@163.com>
2018-08-14 00:58:57 +0800

    路由修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import layout from '@/views/layout/layout'
-import AgGrid from '@/components/AgGrid'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'layout',
-      component: layout
-    },
-    {
-      path: '/AgGrid',
-      name: 'AgGrid',
-      component: AgGrid
+      path: '',
+      component: layout,
+      redirect: 'examples',
+      children: [{
+        path: 'examples',
+        component: () => import('@/views/examples/button')
+      }]
     }
   ]
 })
--- src/views/layout/layout.vue
+++ src/views/layout/layout.vue
@@ -1,32 +1,21 @@
 <template>
   <div id="app-layout">
     <sidebar></sidebar>
-    <ag-grid :row-data="rowData" :column-defs="columnDefs"></ag-grid>
+    <router-view :key="key"></router-view>
   </div>
 </template>
 
 <script>
 import Sidebar from './components/sidebar/sidebar'
-import AgGrid from '../../components/AgGrid'
 
 export default {
   name: 'layout',
   components: {
-    AgGrid,
     Sidebar
   },
-  data () {
-    return {
-      rowData: [
-        {make: 'Toyota', model: 'Celica', price: 35000},
-        {make: 'Ford', model: 'Mondeo', price: 32000},
-        {make: 'Porsche', model: 'Boxter', price: 72000}
-      ],
-      columnDefs: [
-        {headerName: 'Make', field: 'make'},
-        {headerName: 'Model', field: 'model'},
-        {headerName: 'Price', field: 'price'}
-      ]
+  computed: {
+    key() {
+      return this.$route.fullPath
     }
   }
 }
