216caa07ce4be1e97259dbd870b45795f2a10727
LZHD <lzhdfxq@163.com>
2018-09-13 20:37:03 +0800

    backTop

---
+++ src/components/backTop/backTop.vue
@@ -0,0 +1,37 @@
+<template>
+  <BackTop :height="places.height" :right="places.right" :bottom="places.bottom">
+    <div class="backTop"></div>
+  </BackTop>
+</template>
+
+<script>
+  export default {
+    name: "back-top",
+    props: {
+      places: {
+        type: Object,
+        default () {
+          return {
+            height: '200',
+            right: '150',
+            bottom: '100'
+          }
+        }
+      }
+    }
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .backTop {
+    width: 28px;
+    height: 48px;
+    line-height: 48px;
+    background: url(../../assets/img/backTop.png);
+    &:hover {
+       background: url(../../assets/img/backTop_hover.png);
+       opacity: .4;
+       transition: .8s;
+     }
+  }
+</style>
